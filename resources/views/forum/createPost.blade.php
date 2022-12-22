0dac965a66390f7f40040b6334ce2a6385505d20
James Watts <jhmw2002@posteo.net>
2022-12-22 13:29:02 +0000

    Some General Styling Added style and centered the create post div to make it more visually appealing.

--- resources/views/forum/createPost.blade.php
+++ resources/views/forum/createPost.blade.php
@@ -7,20 +7,44 @@
 
 @section('content')
 
-<form method="POST" action=" {{ route('forum.store', ['sub_forum'=>$sub_forum]) }}">
-    @csrf
-    <p>Title: <input type="text" name="title" value="{{ old('title') }}"></p>
-    <p>Body: <input type="text" name="body" value="{{ old('body') }}"></p>
-    <p>UserID: <input type="text" name="user_id" value="{{ old('user_id') }}"></p>
-    <input type="hidden" name="sub_forum_id" value="{{ $sub_forum->id }}">
+<a 
+    class="rounded-md bg-primary-200 p-2 hover:bg-secondary-300 hover:text-primary-100" 
+    href="{{ route('forum.show', ['sub_forum'=>$sub_forum]) }}"
+>
+    back
+</a>
 
-    <input type="submit" value="Submit">
-</form>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <p style="color: red;">{{ $error }}</p>
-    @endforeach
-@endif
+<div class="flex items-center justify-center">
+    <div class="bg-primary-200 flex rounded-md p-2">
+        <form class="px-2 space-y-2" method="POST" rows="4" action=" {{ route('forum.store', ['sub_forum'=>$sub_forum]) }}">
+            @csrf
+            <p>Title: 
+                <input class="p-1 rounded-md bg-primary-100" type="text" name="title" value="{{ old('title') }}">
+            </p>
+            <p>Body:</p> 
+                <input class="p-1 rounded-md bg-primary-100 h-auto w-full" type="text" name="body" value="{{ old('body') }}">
+            <p>UserID: 
+                <input class="p-1 rounded-md bg-primary-100" type="text" name="user_id" value="{{ old('user_id') }}">
+            </p>
+            <input type="hidden" name="sub_forum_id" value="{{ $sub_forum->id }}">
+
+            <div>
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <p style="color: red;">{{ $error }}</p>
+                    @endforeach
+                @endif
+            </div>
+            <div class="flex items-center justify-center w-full">
+                <input 
+                    class="p-2 rounded-md bg-primary-100 hover:bg-secondary-300 hover:text-primary-100" 
+                    type="submit" 
+                    value="Submit"
+                >
+            </div>
+        </form>
+    </div>
+</div>
 
 @endsection
